f067b221f7c9c61edfaca0c245929a5c2240afa4
livemilton <livemilton@hotmail.com>
2022-10-12 04:44:29 -0500

    Clase del 11 de Octubre, C del CRUD

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,7 +1,18 @@
+const producto=require("../models/productos")
 
+//ver la lista de producros
 exports.getProducts=(req,res,next)=> {
     res.status(200).json({
         success:true,
         message: "En esta ruta ud va a poder ver todos los productos"
+    })
+}
+
+//crear nuevo producto /api/productos
+exports.newProduct=async(req,res,next)=>{
+    const product=await producto.create(req.body);
+    res.status(201).json({
+        success:true,
+        product
     })
 }
